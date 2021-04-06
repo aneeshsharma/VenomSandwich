96929723ee185941ad439b1006d38b551d1183d5
soujanya <namburisoujanya@gmail.com>
2021-04-06 20:06:03 +0530

    Changed encryption

--- encrypt.py
+++ encrypt.py
@@ -1,19 +1,15 @@
 import sys
+
+
 def encrypt(src_filename, dest_filename, key):
-    data = open(src_filename, 'rb').read()
-    dst_file = open(dest_filename, 'w')
-    key = str(key)
-    l = len(key)
-    output_str = ""
-    for i in range(len(data)):
-        current = chr(data[i])
-        current_key = key[i % len(key)]
-        output_str += chr(ord(current) ^ ord(current_key))
-    enc_string = '{ 0x' + ', 0x'.join(hex(ord(x))[2:] for x in output_str) + ' };'  
-   # print(enc_string)
-    dst_file.write(enc_string)
-
-
+    plain_text = open(src_filename, 'rb').read()
+    key = bytes(key, 'utf-8')
+    len_key = len(key)
+    encoded = []  
+    for i in range(0, len(plain_text)):
+        encoded.append(plain_text[i] ^ key[i % len_key])
+    dst_file = open(dest_filename, 'wb').write(bytes(encoded))
+    
 #except:
 #    print("File argument needed! %s " % sys.argv[0]) 
 #    sys.exit()
