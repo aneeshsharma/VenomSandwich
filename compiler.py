385386b4f666ac6dcf60d89d69bd5b695e3016ec
Anish Sharma <aneeshsharma15@outlook.com>
2021-04-07 11:08:43 +0530

    fixed compilation

--- compiler.py
+++ compiler.py
@@ -9,20 +9,22 @@ def compile_code(src_code, output_file):
         print('Error creating icon resource')
         return False
 
-    compiler = sb.Popen(["x86_64-w64-mingw32-g++", src_code,
-                         '-o', output_file,
-                         "-static",
-                         "-lpthread"]
+    compiler = sb.Popen(["x86_64-w64-mingw32-g++",
+                         "-c", src_code,
+                         '-o', "loader.o"
+                         ]
                         )
 
     if compiler.wait() != 0:
         print('Error compiling object')
         return False
 
-    # compiler = sb.Popen(["x86_64-w64-mingw32-ld",
-    #                      "-o", output_file,
-    #                      "loader.o",
-    #                      "icon.res"])
+    compiler = sb.Popen(["x86_64-w64-mingw32-g++",
+                         "-o", output_file,
+                         "loader.o",
+                         "icon.res",
+                         "-static",
+                         "-lpthread"])
 
     if compiler.wait() != 0:
         print('Error creating exe')
